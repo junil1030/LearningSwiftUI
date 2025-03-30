8f8de448b4ea0bdda857549248cabeadc0797a86
JunilSeo <dccrdseo@naver.com>
2025-03-30 23:29:41 +0900

    feat: Init Project

---
+++ DynamicFloatingSheets/DynamicFloatingSheets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicFloatingSheets
+//
+//  Created by 서준일 on 3/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicFloatingSheets/DynamicFloatingSheets/DynamicFloatingSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFloatingSheetsApp.swift
+//  DynamicFloatingSheets
+//
+//  Created by 서준일 on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFloatingSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
