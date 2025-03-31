fc42ad840d2372b6ff0a30991454f86a2d775eb2
JunilSeo <dccrdseo@naver.com>
2025-03-31 18:32:53 +0900

    feat: View 생성 및 Interaction 추가

--- DynamicFloatingSheets/DynamicFloatingSheets/ContentView.swift
+++ DynamicFloatingSheets/DynamicFloatingSheets/ContentView.swift
@@ -7,17 +7,91 @@
 
 import SwiftUI
 
+enum CurrentView {
+    case actions
+    case periods
+}
+
 struct ContentView: View {
     //MARK: - 뷰 속성
     @State private var show: Bool = false
+    @State private var currentView: CurrentView = .actions
+    @State private var selectedAction: Action?
+    @State private var selectedPeriod: Period?
     var body: some View {
         Button("Show Tray View") {
             show.toggle()
         }
         .systemTrayView($show) {
-            Text("Hello Form Tray")
-                .frame(maxWidth: .infinity)
-                .frame(height: 300)
+            VStack(spacing: 15) {
+                ZStack {
+                    View1()
+                }
+                .compositingGroup()
+                
+                Button {
+                    
+                } label: {
+                    Text("Continue")
+                        .fontWeight(.semibold)
+                        .frame(maxHeight: .infinity)
+                        .padding(.vertical, 15)
+                        .foregroundStyle(.white)
+                        .background(.blue, in: .capsule)
+                }
+                .padding(.top, 15)
+            }
+            .padding(20)
+        }
+    }
+    
+    //MARK: - View 1
+    @ViewBuilder
+    func View1() -> some View {
+        VStack(spacing: 12) {
+            HStack {
+                Text("Choose Subscription")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Spacer(minLength: 0)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(Color.gray, Color.primary.opacity(0.1))
+                }
+            }
+            .padding(.bottom, 10)
+            
+            ForEach(actions) { action in
+                let isSelected: Bool = selectedAction?.id == action.id
+            
+                HStack(spacing: 10) {
+                    Image(systemName: action.image)
+                        .font(.title)
+                        .frame(width: 40)
+                    
+                    Text(action.title)
+                        .fontWeight(.semibold)
+                    
+                    Spacer(minLength: 0)
+                    
+                    Image(systemName: isSelected ? "checkmark.circle.fill" : "circle.fill")
+                        .font(.title)
+                        .contentTransition(.symbolEffect)
+                        .foregroundStyle(isSelected ? Color.blue : Color.gray.opacity(0.2))
+                }
+                .padding(.vertical, 6)
+                .contentTransition(.rect)
+                .onTapGesture {
+                    withAnimation(.snappy) {
+                        selectedAction = isSelected ? nil :action
+                    }
+                }
+            }
         }
     }
 }
