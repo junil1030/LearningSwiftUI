fb3e9ff78a6b7336d36250dbe246e614f2706a1d
JunilSeo <dccrdseo@naver.com>
2025-04-01 13:01:56 +0900

    feat: FilterCell 구성

--- NetflixClone/NetflixClone/Components/NetflixFilterBarView.swift
+++ NetflixClone/NetflixClone/Components/NetflixFilterBarView.swift
@@ -7,12 +7,89 @@
 
 import SwiftUI
 
+struct FilterModel: Hashable, Equatable {
+    let title: String
+    let isDropdown: Bool
+    
+    static var mockArray: [FilterModel] {
+        [
+            FilterModel(title: "TV Shows", isDropdown: false),
+            FilterModel(title: "Movies", isDropdown: false),
+            FilterModel(title: "Categories", isDropdown: true)
+        ]
+    }
+}
+
 struct NetflixFilterBarView: View {
+    
+    var filters: [FilterModel] = FilterModel.mockArray
+    var selectedFilter: FilterModel? = nil
+    var onFilterPressed: ((FilterModel) -> Void)? = nil
+    var onXMarkPressed: (() -> Void)? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal) {
+            HStack {
+                if selectedFilter != nil {
+                    Image(systemName: "xmark")
+                        .padding(8)
+                        .background(
+                            Circle()
+                                .stroke(style: StrokeStyle(lineWidth: 1))
+                        )
+                        .foregroundStyle(.netflixLightGray)
+                        .background(Color.netflixBlack.opacity(0.001))
+                        .onTapGesture {
+                            onXMarkPressed?()
+                        }
+                        .transition(AnyTransition.move(edge: .leading))
+                        .padding(.leading, 16)
+                }
+                
+                ForEach(filters, id: \.self) { filter in
+                    if selectedFilter == nil || selectedFilter == filter {
+                        NetflixFilterCell(
+                            title: filter.title,
+                            isDropDown: filter.isDropdown,
+                            isSelected: selectedFilter == filter
+                        )
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            onFilterPressed?(filter)
+                        }
+                        .padding(.leading, ((selectedFilter == nil) && filter == filters.first) ? 16 : 0)
+                    }
+                }
+            }
+            .padding(.vertical, 4)
+        }
+        .scrollIndicators(.hidden)
+        .animation(.bouncy, value: selectedFilter)
+    }
+}
+
+fileprivate struct NetflixFilterVarViewPreview: View {
+    
+    @State private var filters = FilterModel.mockArray
+    @State private var selectedFilter: FilterModel?
+    
+    var body: some View {
+        NetflixFilterBarView(
+            filters: filters,
+            selectedFilter: selectedFilter,
+            onFilterPressed: { newFilter in
+                selectedFilter = newFilter
+            },
+            onXMarkPressed: {
+                selectedFilter = nil
+            }
+        )
     }
 }
 
 #Preview {
-    NetflixFilterBarView()
+    ZStack {
+        Color.netflixBlack.ignoresSafeArea()
+        NetflixFilterVarViewPreview()
+    }
 }
