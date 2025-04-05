fbc3aeac6b4ec8f0c587da3936a948ab031fa91e
JunilSeo <dccrdseo@naver.com>
2025-04-05 19:10:39 +0900

    feat: HeroCell 작성

--- NetflixClone/NetflixClone/Components/NetflixHeroCell.swift
+++ NetflixClone/NetflixClone/Components/NetflixHeroCell.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct NetflixHeroCell: View {
+    
+    var imageName: String = "RandomImage"
+    var isNetflixFlim: Bool = true
+    var title: String = "players"
+    var categories: [String] = ["Raunchy", "Romantic", "Comedy"]
+    var onBackgroundPressed: (() -> Void)? = nil
+    var onPlayPressed: (() -> Void)? = nil
+    var onMyListpressed: (() -> Void)? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //ImageLoader
+        }
+            .aspectRatio(0.8, contentMode: .fit)
     }
 }
 
 #Preview {
     NetflixHeroCell()
+        .padding(40)
 }
