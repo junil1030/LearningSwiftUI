837abbc200032475f850894c1a05f8e75f4c816e
JunilSeo <dccrdseo@naver.com>
2025-04-05 19:09:59 +0900

    feat: 공용 함수 작성

---
+++ NetflixClone/NetflixClone/Components/NetflixHeroCell.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixHeroCell.swift
+//  NetflixClone
+//
+//  Created by 서준일 on 4/5/25.
+//
+
+import SwiftUI
+
+struct NetflixHeroCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixHeroCell()
+}
---
+++ NetflixClone/NetflixClone/Shared/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NetflixClone
+//
+//  Created by 서준일 on 4/5/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
