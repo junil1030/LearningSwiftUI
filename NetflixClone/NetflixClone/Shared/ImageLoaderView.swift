68c8fdbd0ba4704db1627ee2d1072bb8d77da0c4
JunilSeo <dccrdseo@naver.com>
2025-04-06 17:47:46 +0900

    feat: SDWebView 사용하여 이미지 출력 셀 작성

--- NetflixClone/NetflixClone/Shared/ImageLoaderView.swift
+++ NetflixClone/NetflixClone/Shared/ImageLoaderView.swift
@@ -1,35 +1,35 @@
-////
-////  ImageLoaderView.swift
-////  NetflixClone
-////
-////  Created by 서준일 on 4/5/25.
-////
 //
-//import SwiftUI
-//import SDWebImageSwiftUI
+//  ImageLoaderView.swift
+//  NetflixClone
 //
-//struct ImageLoaderView: View {
-//    
-//    var urlString: String = Constants.randomImage
-//    var resizingMode: ContentMode = .fill
-//    
-//    var body: some View {
-//        Rectangle()
-//            .opacity(0.001)
-//            .overlay(
-//                WebImage(url: URL(string: urlString))
-//                    .resizable()
-//                    .indicator(.activity)
-//                    .aspectRatio(contentMode: resizingMode)
-//                    .allowsHitTesting(false)
-//            )
-//            .clipped()
-//    }
-//}
+//  Created by 서준일 on 4/5/25.
 //
-//#Preview {
-//    ImageLoaderView()
-//        .cornerRadius(30)
-//        .padding(40)
-//        .padding(.vertical, 60)
-//}
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0.001)
+            .overlay(
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .cornerRadius(30)
+        .padding(40)
+        .padding(.vertical, 60)
+}
