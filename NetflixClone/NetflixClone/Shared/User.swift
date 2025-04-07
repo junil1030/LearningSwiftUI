ca40ea263673649bbe8cdf463b595a9a9312b4f0
JunilSeo <dccrdseo@naver.com>
2025-04-07 16:59:52 +0900

    feat: Product, User 모델 제작

---
+++ NetflixClone/NetflixClone/Shared/User.swift
@@ -0,0 +1,39 @@
+//
+//  User.swift
+//  NetflixClone
+//
+//  Created by 서준일 on 4/7/25.
+//
+
+import Foundation
+
+struct UserArray: Codable {
+    let users: [User]
+    let total, skip, limit: Int
+}
+
+struct User: Codable, Identifiable {
+    let id: Int
+    let firstName, lastName: String
+    let age: Int
+    let email, phone, username, password: String
+    let image: String
+    let height: Double
+    let weight: Double
+    
+    static var mock: User {
+        User(
+            id: 444,
+            firstName: "Nick",
+            lastName: "Sarno",
+            age: 76,
+            email: "hi@hi.com",
+            phone: "",
+            username: "",
+            password: "",
+            image: Constants.randomImage,
+            height: 180,
+            weight: 200
+        )
+    }
+}
