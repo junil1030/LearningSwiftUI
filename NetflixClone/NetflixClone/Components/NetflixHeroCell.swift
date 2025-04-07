386f52bd40d7371dac4e80aca1236c981f9890ea
JunilSeo <dccrdseo@naver.com>
2025-04-07 17:01:34 +0900

    feat: HeroCell 작성

--- NetflixClone/NetflixClone/Components/NetflixHeroCell.swift
+++ NetflixClone/NetflixClone/Components/NetflixHeroCell.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct NetflixHeroCell: View {
     
@@ -18,15 +19,89 @@ struct NetflixHeroCell: View {
     var onMyListpressed: (() -> Void)? = nil
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             ImageLoaderView(urlString: imageName)
             
             VStack(spacing: 16) {
-                Text("hi")
+                VStack(spacing: 0) {
+                    if isNetflixFlim {
+                        HStack(spacing: 8) {
+                            Text("N")
+                                .foregroundStyle(.netflixRed)
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                            
+                            Text("Film")
+                                .kerning(3)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.netflixWhite)
+                        }
+                    }
+                    
+                    Text(title)
+                        .font(.system(size:50, weight: .medium, design: .serif))
+                }
+                
+                HStack(spacing: 8) {
+                    ForEach(categories, id: \.self) { category in
+                        Text(category)
+                            .font(.callout)
+                        
+                        if category != categories.last {
+                            Circle()
+                                .frame(width:4, height: 4)
+                        }
+                    }
+                }
+                
+                HStack(spacing: 16) {
+                    HStack {
+                        Image(systemName: "play.fill")
+                        Text("Play")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.netflixDarkGray)
+                    .background(.netflixWhite)
+                    .cornerRadius(4)
+                    .asButton(.press) {
+                        onPlayPressed?()
+                    }
+                    
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("My List")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.netflixWhite)
+                    .background(.netflixDarkGray)
+                    .cornerRadius(4)
+                    .asButton(.press) {
+                        onMyListpressed?()
+                    }
+                }
+                .font(.callout)
+                .fontWeight(.medium)
             }
-            .background(Color.red)
+            .padding(24)
+            .background(
+                LinearGradient(
+                    colors: [
+                        .netflixBlack.opacity(0),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4),
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom)
+            )
         }
+        .foregroundStyle(.netflixWhite)
+        .cornerRadius(10)
         .aspectRatio(0.8, contentMode: .fit)
+        .asButton(.tap) {
+            onBackgroundPressed?()
+        }
     }
 }
 
